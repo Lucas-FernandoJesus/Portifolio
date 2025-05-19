18d3deada79309edbbc34d82fb7d0991548eac17
Lucas-FernandoJesus <lucas_fernando-jesus@proton.me>
2025-05-18 21:03:50 -0300

    Incluido função de mensagem no e-mail, ajustado estilização e atualizado link projeto 1

---
+++ assets/JavaScript/index.js
@@ -0,0 +1,3 @@
+document.getElementById("emailButton").addEventListener("click", function () {
+  window.location.href = "mailto:lucas_fernando-jesus@proton.me?subject=Projeto para ser desenvolvido&body=Olá Lucas,%0D%0A%0D%0AAcessei o seu portfólio e gostaria de entrar em contato porque tenho um projeto para desenvolver.%0D%0A%0D%0AFico no aguardo do seu retorno.%0D%0A%0D%0AObrigado!";
+});
